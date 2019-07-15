31097556bdd120a4f74f84ca89222593903f4ba7
Roman Zimmermann <roman@more-onion.com>
2019-07-15 16:54:16 +0200

    Add PaymentFactoryTest.

---
+++ tests/PaymentFactoryTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\webform_paymethod_select;
+
+use Drupal\little_helpers\Webform\Submission;
+use Upal\DrupalUnitTestCase;
+
+/**
+ * Test the payment factory.
+ */
+class PaymentFactoryTest extends DrupalUnitTestCase {
+
+  /**
+   * Create a mock submission to get values from.
+   */
+  protected function submissionStub($cid_values, $key_values) {
+    $submission = $this->createMock(Submission::class);
+    $submission->method('valueByCid')->will($this->returnCallback(function ($cid) use ($cid_values) {
+      if (isset($cid_values[$cid])) {
+        return $cid_values[$cid];
+      }
+    }));
+    $submission->method('valueByKey')->will($this->returnCallback(function ($key) use ($key_values) {
+      if (isset($key_values[$key])) {
+        return $key_values[$key];
+      }
+    }));
+    return $submission;
+  }
+
+  /**
+   * Scenario where everything is read from components.
+   */
+  public function testReadFromComponent() {
+    $component['extra'] = [
+      'currency_code_source' => 'component',
+      'currency_code_component' => 1,
+      'line_items' => [
+        new PaymethodLineItem([
+          'name' => 'first_item',
+          'description' => 'First item',
+          'amount_source' => 'component',
+          'amount_component' => 2,
+          'quantity_source' => 'component',
+          'quantity_component' => 3,
+        ]),
+      ],
+    ];
+    $cid_values = [
+      1 => 'EUR',
+      2 => 3.0,
+      3 => 5,
+    ];
+    $factory = new PaymentFactory($component);
+    $payment = entity_create('payment', []);
+    $factory->updatePayment($payment, $this->submissionStub($cid_values, []));
+    $this->assertEqual('EUR', $payment->currency_code);
+    $this->assertEqual(15.0, $payment->totalAmount(TRUE));
+  }
+
+  /**
+   * Read recurrence from donation interval.
+   */
+  public function testReadFromDonationInterval() {
+    $component['extra'] = [
+      'currency_code_source' => 'fixed',
+      'currency_code' => 'EUR',
+      'line_items' => [
+        new PaymethodLineItem([
+          'name' => 'first_item',
+          'description' => 'First item',
+          'amount_source' => 'fixed',
+          'amount' => 3.0,
+          'quantity_source' => 'fixed',
+          'quantity' => 5,
+        ]),
+      ],
+    ];
+    $factory = new PaymentFactory($component);
+    $payment = entity_create('payment', []);
+    $submission = $this->createMock(Submission::class);
+    $key_values['donation_interval'] = 'm';
+    $factory->updatePayment($payment, $this->submissionStub([], $key_values));
+    $this->assertEqual(15.0, $payment->totalAmount(TRUE));
+    $this->assertEqual([
+      'interval_unit' => 'monthly',
+      'interval_value' => 1,
+    ], (array) $payment->line_items['first_item']->recurrence);
+  }
+
+}
