ca05b57d89e63dba5b8f04a31a33bce88f68b230
Roman Zimmermann <roman@more-onion.com>
2020-02-13 10:06:51 +0100

    Remove deprecated serialization.

--- src/WebformPaymentContext.php
+++ src/WebformPaymentContext.php
@@ -40,34 +40,6 @@ class WebformPaymentContext implements PaymentContextInterface {
     }
   }
 
-  /**
-   * @deprecated Serializing contexts is not a good idea. Use toContext()
-   *   and fromContext() instead.
-   */
-  public function __sleep() {
-    $this->_cid = $this->component['cid'];
-    return array('submission', 'component');
-  }
-
-  /**
-   * @deprecated @see __sleep().
-   */
-  public function __wakeup() {
-    if (!$this->submission && isset($this->nid) && isset($this->sid)) {
-      $this->submission = Submission::load($this->nid, $this->sid);
-      if (!$this->submission) {
-        throw new \UnexpectedValueException('Submission seems to have vanished');
-      }
-    }
-    if (isset($this->_cid)) {
-      $this->component = &$this->submission->webform->component($this->_cid);
-    }
-    else {
-      $components = $this->submission->webform->componentsByType('paymethod_select');
-      $this->component = reset($components);
-    }
-  }
-
   public function getSubmission() {
     return $this->submission;
   }
