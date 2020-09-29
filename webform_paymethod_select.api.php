80f3d90fd39638d7f938d8837c5d8856b0921ee5
Roman Zimmermann <roman@more-onion.com>
2020-09-29 18:10:56 +0200

    Add hook_webform_paymethod_select_payment_alter().

--- webform_paymethod_select.api.php
+++ webform_paymethod_select.api.php
@@ -5,6 +5,8 @@
  * Document hooks invoked by this module.
  */
 
+use Drupal\little_helpers\Webform\Submission;
+
 /**
  * Alter the available payment methods for a payment.
  *
@@ -20,6 +22,25 @@ function hook_webform_paymethod_select_method_list(array &$methods, \Payment $pa
       if ($method->controller->name == 'cash') {
         unset($methods[$pmid]);
       }
+    }
+  }
+}
+
+/**
+ * Alter how the payment is generated based on the submission.
+ *
+ * @param \Payment $payment
+ *   The payment to be altered.
+ * @param array $component
+ *   The webform component for which the payment is generated.
+ * @param \Drupal\little_helpers\Webform\Submission $submission
+ *   The submission for which the payment is generated.
+ */
+function hook_webform_paymethod_select_payment_alter(\Payment &$payment, array $component, Submission $submission) {
+  // Example: Remove line items that have a zero quantity or a zero amount.
+  foreach ($payment->line_items as $name => $item) {
+    if ($item->quantity == 0 || $item->totalAmount() == 0) {
+      unset($payment->line_items[$name]);
     }
   }
 }
